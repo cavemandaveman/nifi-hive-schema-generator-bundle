2899dc8b4f5495ea1148028288f992bb756956d2
cavemandaveman <cavemandaveman@protonmail.com>
2018-03-22 10:23:15 -0500

    Clean up and extract logic out to its own class

---
+++ nifi-hive-schema-generator-processors/src/main/scala/com/nineteen04labs/nifi/processors/CreateHQL.scala
@@ -0,0 +1,118 @@
+package com.nineteen04labs.nifi.processors
+
+import java.io._
+import play.api.libs.json._
+
+class CreateHQL(stream: InputStream) {
+
+  val br = new BufferedReader(new InputStreamReader(stream))
+
+  var line: String = null
+  var lines = 0
+  var schema: JsValue = Json.obj()
+
+  while ({
+    line = br.readLine()
+    line != null
+  }) {
+    lines += 1
+    schema = merge(schema, Json.parse(line))
+  }
+  stream.close()
+
+  case class RowMismatch(a: JsValue, b: JsValue) extends Throwable {
+    override def toString = Seq(
+      s"On the line $lines you attempted to insert this JSON:",
+      Json.prettyPrint(b),
+      "with the corresponding schema:",
+      out(b),
+      "into the schema with this signature:",
+      out(a)) mkString "\n"
+  }
+
+  case class InconsistentArray(arr: Seq[JsValue]) extends Throwable {
+    override def toString =
+      "You have an array containing incompatible datatypes:" + Json.prettyPrint(JsArray(arr))
+  }
+
+  private def prepare(arr: JsValue) = arr match {
+    case x @ JsArray(Seq(_)) => x
+    case JsArray(s) =>
+      try
+        JsArray(Seq(s.foldLeft(JsNull: JsValue)(merge(_, _))))
+      catch {
+        case RowMismatch(_, _) =>
+          throw new InconsistentArray(s)
+      }
+    case x => x
+  }
+
+  def merge(a: JsValue, b: JsValue): JsValue = {
+    (a, prepare(b)) match {
+      case (JsNull, x) => x
+      case (x, JsNull) => x
+      case (x: JsBoolean, _: JsBoolean) => x
+
+      case (a @ JsString(ax), b @ JsString(bx)) =>
+        if (ax.size > bx.size) a else b
+
+      case (JsNumber(ax), JsNumber(bx)) => JsNumber((ax max bx) setScale (ax.scale max bx.scale))
+      case (JsArray(ax), JsArray(bx)) => JsArray(Seq(merge(ax.head, bx.head)))
+
+      case (JsObject(ax), JsObject(bx)) =>
+        JsObject((ax.toSeq ++ bx.toSeq).groupBy(_._1).mapValues {
+          case Seq((_, x)) => prepare(x)
+          case Seq((_, ax), (_, bx)) => merge(ax, bx)
+        }.toSeq)
+
+      case _ => throw new RowMismatch(a, b)
+    }
+  }
+
+  def out(json: JsValue, i: Int = 0, key: Option[String] = None): String = {
+    val pad = "\t" * i
+    pad + key.fold("")(_ + " ") + (json match {
+      case JsNull => "???"
+      case _: JsBoolean => "BOOLEAN"
+
+      case JsString(x) if 0 < x.size && x.size < 65356 =>
+        s"VARCHAR(${x.size})"
+      case _: JsString => "STRING"
+
+      case JsNumber(x) if (x.scale == 0) =>
+        if (x.isValidByte) "TINYINT"
+        else if (x.isValidShort) "SMALLINT"
+        else if (x.isValidInt) "INT"
+        else if (x.isValidLong) "BIGINT"
+        else s"NUMERIC(${x.precision}, 0)"
+      case JsNumber(x) if (x.precision <= 7) => "FLOAT"
+      case JsNumber(x) if (x.precision <= 15) => "DOUBLE"
+      case JsNumber(x) => s"NUMERIC(${x.precision}, ${x.scale})"
+
+      case JsArray(x) =>
+        Seq(
+          "ARRAY<", out(x.head, i + 1), s"$pad>") mkString "\n"
+
+      case JsObject(x) =>
+        (Seq("STRUCT<") ++ x.map {
+          case (k, v) =>
+            out(v, i + 1, Some(k + ":"))
+        } ++ Seq(s"$pad>")).mkString("\n")
+    })
+  }
+
+  def definition(i: Int = 0) = schema match {
+    case JsObject(x) =>
+      x.map {
+        case (k, v) => out(v, i, Some(k))
+      } mkString ",\n"
+    case _ => "ERROR"
+  }
+
+  def table(name: String, location: String) = Seq(
+    s"CREATE EXTERNAL TABLE $name (",
+    definition(1).replace('.', '_'),
+    ") ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.JsonSerde';",
+    s"location '$location';").mkString("\n")
+
+}
--- nifi-hive-schema-generator-processors/src/main/scala/com/nineteen04labs/nifi/processors/HiveSchemaGenerator.scala
+++ nifi-hive-schema-generator-processors/src/main/scala/com/nineteen04labs/nifi/processors/HiveSchemaGenerator.scala
@@ -16,15 +16,8 @@
  */
 package com.nineteen04labs.nifi.processors
 
-import java.io._
-import java.util.concurrent.atomic.AtomicReference
-
-// Commons IO
-import org.apache.commons.io.IOUtils
-
 // NiFi
 import org.apache.nifi.components.PropertyDescriptor
-import org.apache.nifi.processor.io.InputStreamCallback
 import org.apache.nifi.processor.{ AbstractProcessor, Relationship }
 import org.apache.nifi.processor.{ ProcessorInitializationContext, ProcessContext, ProcessSession }
 import org.apache.nifi.annotation.behavior.{ ReadsAttribute, ReadsAttributes }
@@ -74,136 +67,10 @@ class HiveSchemaGenerator extends AbstractProcessor with HiveSchemaGeneratorProp
             .evaluateAttributeExpressions(inFlowFile)
             .getValue
 
-        val content = new AtomicReference[String]
-        session.read(inFlowFile, new InputStreamCallback {
-          override def process(in: InputStream): Unit = {
-            try {
-              val s = IOUtils.toString(in)
-              content.set(s)
-            } catch {
-              case t: Throwable =>
-                getLogger().error(t.getMessage, t)
-                session.transfer(inFlowFile, RelFailure)
-            }
-          }
-        })
+        val content = session.read(inFlowFile)
 
-        class CreateHQL(stream: InputStream) {
-          import play.api.libs.json._
+        val hql = new CreateHQL(content).table(tableName, location)
 
-          val br = new BufferedReader(new InputStreamReader(stream))
-
-          var line: String = null
-          var lines = 0
-          var schema: JsValue = Json.obj()
-          while ({
-            line = br.readLine()
-            line != null
-          }) {
-            lines += 1
-            schema = merge(schema, Json.parse(line))
-          }
-          stream.close()
-
-          case class RowMismatch(a: JsValue, b: JsValue) extends Throwable {
-            override def toString = Seq(
-              s"On the line $lines you attempted to insert this JSON:",
-              Json.prettyPrint(b),
-              "with the corresponding schema:",
-              out(b),
-              "into the schema with this signature:",
-              out(a)) mkString "\n"
-          }
-
-          case class InconsistentArray(arr: Seq[JsValue]) extends Throwable {
-            override def toString =
-              "You have an array containing incompatible datatypes:" + Json.prettyPrint(JsArray(arr))
-          }
-
-          private def prepare(arr: JsValue) = arr match {
-            case x @ JsArray(Seq(_)) => x
-            case JsArray(s) =>
-              try
-                JsArray(Seq(s.foldLeft(JsNull: JsValue)(merge(_, _))))
-              catch {
-                case RowMismatch(_, _) =>
-                  throw new InconsistentArray(s)
-              }
-            case x => x
-          }
-
-          def merge(a: JsValue, b: JsValue): JsValue = {
-            (a, prepare(b)) match {
-              case (JsNull, x) => x
-              case (x, JsNull) => x
-              case (x: JsBoolean, _: JsBoolean) => x
-
-              case (a @ JsString(ax), b @ JsString(bx)) =>
-                if (ax.size > bx.size) a else b
-
-              case (JsNumber(ax), JsNumber(bx)) => JsNumber((ax max bx) setScale (ax.scale max bx.scale))
-              case (JsArray(ax), JsArray(bx)) => JsArray(Seq(merge(ax.head, bx.head)))
-
-              case (JsObject(ax), JsObject(bx)) =>
-                JsObject((ax.toSeq ++ bx.toSeq).groupBy(_._1).mapValues {
-                  case Seq((_, x)) => prepare(x)
-                  case Seq((_, ax), (_, bx)) => merge(ax, bx)
-                }.toSeq)
-
-              case _ => throw new RowMismatch(a, b)
-            }
-          }
-
-          def out(json: JsValue, i: Int = 0, key: Option[String] = None): String = {
-            val pad = "\t" * i
-            pad + key.fold("")(_ + " ") + (json match {
-              case JsNull => "???"
-              case _: JsBoolean => "BOOLEAN"
-
-              case JsString(x) if 0 < x.size && x.size < 65356 =>
-                s"VARCHAR(${x.size})"
-              case _: JsString => "STRING"
-
-              case JsNumber(x) if (x.scale == 0) =>
-                if (x.isValidByte) "TINYINT"
-                else if (x.isValidShort) "SMALLINT"
-                else if (x.isValidInt) "INT"
-                else if (x.isValidLong) "BIGINT"
-                else s"NUMERIC(${x.precision}, 0)"
-              case JsNumber(x) if (x.precision <= 7) => "FLOAT"
-              case JsNumber(x) if (x.precision <= 15) => "DOUBLE"
-              case JsNumber(x) => s"NUMERIC(${x.precision}, ${x.scale})"
-
-              case JsArray(x) =>
-                Seq(
-                  "ARRAY<", out(x.head, i + 1), s"$pad>") mkString "\n"
-
-              case JsObject(x) =>
-                (Seq("STRUCT<") ++ x.map {
-                  case (k, v) =>
-                    out(v, i + 1, Some(k + ":"))
-                } ++ Seq(s"$pad>")).mkString("\n")
-            })
-          }
-
-          def definition(i: Int = 0) = schema match {
-            case JsObject(x) =>
-              x.map {
-                case (k, v) => out(v, i, Some(k))
-              } mkString ",\n"
-            case _ => "ERROR"
-          }
-
-          def table(name: String, location: String) = Seq(
-            s"CREATE EXTERNAL TABLE $name (",
-            definition(1).replace('.', '_'),
-            ") ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.JsonSerde';",
-            s"location '$location';").mkString("\n")
-
-        }
-
-        val in = session.read(inFlowFile)
-        val hql = new CreateHQL(in).table(tableName, location)
         session.putAttribute(inFlowFile, "hiveql-statement", hql)
       }
       case _ =>
